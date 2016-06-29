4aed848beaa5d63d6764bdbd255513f4fec9d51b
Sinisa Nimcevic <sinimch@gmail.com>
2016-06-29 12:08:05 +0100

    rely on single variables instead of an array for date model

--- app/app.js
+++ app/app.js
@@ -8,13 +8,12 @@
 	doubleDatePicker.controller('MainController', ['$scope', 
 		function MainController($scope){
 			var vm = this;
-			vm.someDates = ['', ''];
-			vm.date1 = vm.someDates[0];
-			vm.date2 = vm.someDates[1];
+			
+			vm.dateFrom = '';
+			vm.dateTo = '';
 
-			vm.someOtherDates = ['', ''];
-			vm.dateO1 = vm.someOtherDates[0];
-			vm.dateO2 = vm.someOtherDates[1];
+ 			vm.dateFromX = '';
+			vm.dateToX = '';
 		}
 	]);
 
--- app/dateRangePicker.directive.js
+++ app/dateRangePicker.directive.js
@@ -14,7 +14,8 @@
     	return {
     		templateUrl : 'date-range-picker.html',
     		scope : {
-    			dates: '='
+    			dateFrom: '=dateFrom',
+    			dateTo: '=dateTo'
     		},
     		link : function (scope, element, attrs) {
     			// link function
@@ -25,10 +26,10 @@
 	    			vm.days = 0;
 
 					vm.setDateModel = function(i, date) {
-						vm.dates[i] = date;					
-						if (vm.dates[0].length > 0 && vm.dates[1].length > 0) {
-							var a = new Date(vm.dates[0]);
-							var b = new Date(vm.dates[1]);
+						i == 0 ? vm.dateFrom = date : vm.dateTo = date;				
+						if (vm.dateFrom.length > 0 && vm.dateTo > 0) {
+							var a = new Date(vm.dateFrom);
+							var b = new Date(vm.dateTo);
 							vm.days = Math.round(Math.abs((a.getTime() - b.getTime())/(24*60*60*1000)));
 						} else {
 							vm.days = 0;
@@ -46,40 +47,49 @@
 							onSelect: function(selectedDate) {
 								// general variables
 								var clickedDate = $.datepicker.parseDate( "D d MM yy", selectedDate );
-								var fromDate = $.datepicker.parseDate( "D d MM yy", vm.dates[0] );
-								var toDate = $.datepicker.parseDate( "D d MM yy", vm.dates[1] );
+								var fromDate = $.datepicker.parseDate( "D d MM yy", vm.dateFrom );
+								var toDate = $.datepicker.parseDate( "D d MM yy", vm.dateTo );
 								var reset = false;
 								var origin = vm.origin || 'from';
 
 								// check if it's a click on an existing date (reset a value and end the loop)
-								for (var i=0; i<2; i++) {
-									if (vm.dates[i].length > 0) {
-										var compare = new Date(vm.dates[i]);
-										if (clickedDate.getTime() == compare.getTime()) {															
-											vm.setDateModel(i, '');
-											reset = true;
-										}
+
+								if (vm.dateFrom.length > 0) {
+									console.log('here');
+									var compare = new Date(vm.dateFrom);
+									if (clickedDate.getTime() == compare.getTime()) {															
+										vm.setDateModel(0, '');
+										reset = true;
 									}
 								}
+
+								if (vm.dateTo.length > 0) {
+									var compare = new Date(vm.dateTo);
+									if (clickedDate.getTime() == compare.getTime()) {															
+										vm.setDateModel(1, '');
+										reset = true;
+									}
+								}
+
 
 								// add new value, selected date isn't an existing one
 								if (!reset) {
 									// if no "from" value is set, and you navigated from a "from" field
-									if (vm.dates[0].length == 0 && origin == 'from') {	
-										if (vm.dates[1].length == 0) { 
+									if (vm.dateFrom.length == 0 && origin == 'from') {	
+										if (vm.dateTo.length == 0) { 
 											vm.setDateModel(0, selectedDate);
 										} else { // handle existing "to" field
-											var compareTo = new Date(vm.dates[1]);
+											var compareTo = new Date(vm.dateTo);
 											var compareClicked = new Date(selectedDate);
 
 											if (compareTo.getTime() < compareClicked.getTime()) {
-												vm.setDateModel(0, vm.dates[1]);
+												vm.setDateModel(0, vm.dateTo);
 												vm.setDateModel(1, selectedDate);
 											} else { vm.setDateModel(0, selectedDate); }
 										}
 									} 
 									// if no "to" value is set, and you navigated from a "to" field
-									else if (vm.dates[1].length == 0 && origin == 'to') {
+									else if (vm.dateTo.length == 0 && origin == 'to') {
 										vm.setDateModel(1, selectedDate);
 									}
 									//if both a "from" and a "to" value are set
@@ -91,8 +101,8 @@
 										// assume user is trying to extend the "from" date
 										if (clickedDate < fromDate) { 
 											// allow reverse selection, first the "to" and then the "from" date
-											if (vm.dates[1].length == 0) {
-												vm.setDateModel(1, vm.dates[0]);
+											if (vm.dateTo.length == 0) {
+												vm.setDateModel(1, vm.dateFrom);
 												vm.setDateModel(0, selectedDate);						
 											} else {
 												vm.setDateModel(0, selectedDate);
@@ -112,7 +122,7 @@
 					}
 
 					vm.highlightDays = function (date) {
-						var from = new Date(vm.dates[0]), to = new Date(vm.dates[1]);
+						var from = new Date(vm.dateFrom), to = new Date(vm.dateTo);
 				        for (var i = 0; i < 2; i++) {
 				            if (date >= from && date <= to || date.getTime() == from.getTime() || date.getTime() == to.getTime()) {              
 				                return [true, 'from-to-highlight'];
